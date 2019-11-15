66380b9af139a965d4c4172d67cbab9b3520b3a4
JaydenWev <jaydenwev@gmail.com>
2019-11-15 14:02:26 +0100

    imports update

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_app/pages/choose_location.dart';
-import 'package:flutter_app/pages/home.dart';
-import 'package:flutter_app/pages/loading.dart';
+import 'package:shifter_app/pages/choose_location.dart';
+import 'package:shifter_app/pages/home.dart';
+import 'package:shifter_app/pages/loading.dart';
 
 void main() => runApp(MaterialApp(
   initialRoute: '/home',
